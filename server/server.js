2371b92926bfc3d37e359e37d558ef13464d246d
Rohit-Bishnoi29 <rohit_12013099@nitkkr.ac.in>
2023-07-07 19:06:26 +0530

    imp

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 const dbConfig = require("./config/dbConfig");
 app.use(express.json());
 
